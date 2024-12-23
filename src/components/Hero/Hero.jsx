b06174d53b85528ace5f7f98b5992a56cd5cdcdc
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-23 11:53:40 +0530

    about

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import heroImg from '/assets/hero/heroImage.png'
+import styles from './Hero.module.css'
+function Hero(){
+    return(
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I’m Simran</h1>
+            <p className={styles.description}>I’m a Software Engineer and Web Developer who turns ideas into modern, user-friendly websites and apps that stand out</p>
+            <a href="mailto:simrankasare5@gmail.com" className={styles.contactBtn}>Contact Me</a>
+        </div>
+        <img src={heroImg} alt='hero image for me' className={styles.heroImg}/>
+        <div className={StyleSheet.topBlur}/>
+        <div className={StyleSheet.bottomBlur}/>
+
+    </section>)
+}
+
+export default Hero
