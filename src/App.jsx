6cb9380139c619dfbb00fe0052c51ba3258d450f
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-23 19:48:21 +0530

    rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import style from './App.module.css'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
+import About from './components/About/About.jsx';
 
 function App() {
   return(<div className={style.App}>
     <Navbar/>
     <Hero/>
+    <About/>
   </div>
   )
 }
