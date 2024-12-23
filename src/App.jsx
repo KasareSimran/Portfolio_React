a6b3ea585f62dd9eb4e127c1d7bff28b307c86b4
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-23 20:39:53 +0530

    rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import style from './App.module.css'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
 import About from './components/About/About.jsx';
+import Experience from './components/Experience/Experience.jsx';
 
 function App() {
   return(<div className={style.App}>
     <Navbar/>
     <Hero/>
     <About/>
+    <Experience/>
   </div>
   )
 }
