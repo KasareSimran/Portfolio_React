18c8441f04e02ef5c6a28c73e48c8f6c67f7a31d
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-23 11:51:12 +0530

    Component rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import style from './App.module.css'
-import Navbar from './components/Navbar/Navbar.jsx'
+import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
 
 function App() {
