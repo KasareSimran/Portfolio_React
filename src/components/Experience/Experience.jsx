6a5c53bbed19fd6cb9d2bccb48c458aa0b1b4be8
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-24 15:59:17 +0530

    final update for png

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,27 @@
 
 import styles from './Experience.module.css'
+import skills from '../../data/skills.json'
 function Experience(){
     return(
-        <section></section>
+        <section id='experience'>
+            <h2>Experience</h2>
+            <div>
+                <div>
+                    {
+                        skills.map((skill,id)=>{
+                            return (
+                            <div key={id} >
+                                <div><img src={skill.imageSrc} alt={skill.title}/></div>
+                                <p>{skill.title}</p>   
+                            </div>
+
+                            )
+                        })
+                    }
+                </div>
+                <ul></ul>
+            </div>
+        </section>
     )
 }
 export default Experience
