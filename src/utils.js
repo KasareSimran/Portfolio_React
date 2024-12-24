2255e1d72909a5c5af846f2f83f28237fd0f7081
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-24 18:01:57 +0530

    style for project section

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 
 export const getImgUrl = (path) => {
   const url = new URL(`/assets/${path}`, import.meta.url).href;
+  console.log("hi")
   console.log(url); // Debug: Check the resolved URL
  
 };
