2b02fbc5b2d26a9fbc54c3a6286c946379ba317d
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-24 16:30:20 +0530

    data for skills,history

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,7 @@
 
 import styles from './Experience.module.css'
 import skills from '../../data/skills.json'
+import history from '../../data/history.json'
 function Experience(){
     return(
         <section id='experience'>
@@ -19,7 +20,27 @@ function Experience(){
                         })
                     }
                 </div>
-                <ul></ul>
+                <ul>
+                {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <img
+                  src={historyItem.imageSrc}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+                </ul>
             </div>
         </section>
     )
